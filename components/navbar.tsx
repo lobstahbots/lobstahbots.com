be1027858d8878c8cb6a190da5379f23fe4ffcd7
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2022-11-11 11:13:26 -0500

    start gallery

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,13 +9,13 @@ export default function Navbar () {
           <a className = {styles.title}>Lobstah Bots</a>
         </div>
         <div className = {styles.menu}>
-          <a href = "/" target="_blank" rel="noopener noreferrer" className = {styles.menuOption}>Home</a>
-          <a href = "/history" target="_blank" rel="noopener noreferrer" className = {styles.menuOption}>History</a>
-          <a href = "/team" target="_blank" rel="noopener noreferrer" className = {styles.menuOption}>Our Team</a>
-          <a href = "/gallery" target="_blank" rel="noopener noreferrer" className = {styles.menuOption}>Gallery</a>
-          <a href = "/contact" target="_blank" rel="noopener noreferrer" className = {styles.menuOption}>Contact</a>
-          <a href = "/newsletter" target="_blank" rel="noopener noreferrer" className = {styles.menuOption}>Newsletter</a>
-          <a href = "/sponsors" target="_blank" rel="noopener noreferrer" className = {styles.menuOption}>Sponsors</a>
+          <a href = "/" className = {styles.menuOption}>Home</a>
+          <a href = "/history" className = {styles.menuOption}>History</a>
+          <a href = "/team" className = {styles.menuOption}>Our Team</a>
+          <a href = "/gallery" className = {styles.menuOption}>Gallery</a>
+          <a href = "/contact" className = {styles.menuOption}>Contact</a>
+          <a href = "/newsletter" className = {styles.menuOption}>Newsletter</a>
+          <a href = "/sponsors" className = {styles.menuOption}>Sponsors</a>
         </div>
       </nav>
     </header>
--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,7 +1,28 @@
+import styles from "./gallery.module.css";
+import Image from "next/image";
+
 export default function Gallery () {
   return(
-    <div>
-      Gallery
+    <div> 
+      <div className={styles.gallery}>
+          <Image src="/images/lobstahbots.png" alt="Cinque Terre" width="600" height="400"/>
+        <div className={styles.desc}>Add a description of the image here</div>
+      </div>
+
+      <div className={styles.gallery}>
+          <Image src="/images/lobstahbots.png" alt="Forest" width="600" height="400"/>
+        <div className={styles.desc}>Add a description of the image here</div>
+      </div>
+
+      <div className={styles.gallery}>
+          <Image src="/images/lobstahbots.png" alt="Northern Lights" width="600" height="400"/>
+        <div className={styles.desc}>Add a description of the image here</div>
+      </div>
+
+      <div className={styles.gallery}>
+          <Image src="/images/lobstahbots.png" alt="Mountains" width="600" height="400"/>
+        <div className={styles.desc}>Add a description of the image here</div>
+      </div>
     </div>
   );
 }
