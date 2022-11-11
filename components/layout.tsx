4ab27d3d2cb10286670a1cab307b929567a00718
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2022-11-11 09:11:52 -0500

    Migrate back to stable Nextjs pages directory

---
+++ components/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./footer";
+import styles from "./layout.module.css";
+import Navbar from "./navbar";
+
+export default function App ({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.layout}>
+      <Navbar />
+      <div className={styles.content}>
+        { children }
+      </div>
+      <Footer />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import react from "react";
-import styles from "./styles.module.css";
-import "./page.css"
+import styles from "./index.module.css";
 
 export default function Page () {
   return (
@@ -10,14 +8,14 @@ export default function Page () {
       <p>This website is a work-in-progress. Please check back later!</p>
       <div className = {styles.blurb}>
         <h2>Who Are We?</h2>
-        <div>We are FIRST® Robotics Competition (FRC) Team 246 “Lobstah Bots”, a high school robotics team affiliated with Boston University Academy. We are one of the oldest FRC teams, and are now entering our 24th season. The team has competed annually since 1999. 
+        <div>We are FIRST® Robotics Competition (FRC) Team 246 “Lobstah Bots”, a high school robotics team affiliated with Boston University Academy. We are one of the oldest FRC teams, and are now entering our 24th season. The team has competed annually since 1999.
         </div>
         <h3>What is FIRST?</h3>
-        <div>FIRST (For Inspiration and Recognition of Science and Technology) was founded in 1992 by Dean Kamen, inventor of the Segway. The mission of FIRST is to inspire young people to be science and technology leaders, by engaging them in exciting programs that build STEM skills and teach self-confidence, communication, and leadership. 
-The FIRST Robotics Competition is an international program consisting of thousands of teams and tens of thousands of students. In FRC, students have just a few weeks to design, build, and code a 100+ pound robot that can compete in a complex engineering challenge. FRC gives students exposure to engineering, technical design, business management, marketing and many more skills that can be used in college and industry. Most FIRST alumni go to a 4-year college and end up employed in the STEM industry. 
+        <div>FIRST (For Inspiration and Recognition of Science and Technology) was founded in 1992 by Dean Kamen, inventor of the Segway. The mission of FIRST is to inspire young people to be science and technology leaders, by engaging them in exciting programs that build STEM skills and teach self-confidence, communication, and leadership.
+The FIRST Robotics Competition is an international program consisting of thousands of teams and tens of thousands of students. In FRC, students have just a few weeks to design, build, and code a 100+ pound robot that can compete in a complex engineering challenge. FRC gives students exposure to engineering, technical design, business management, marketing and many more skills that can be used in college and industry. Most FIRST alumni go to a 4-year college and end up employed in the STEM industry.
         </div>
         <a href = "https://www.youtube.com/watch?v=-SjPVVeNdKY" target="_blank" rel="noopener noreferrer">
-          <button>Learn More </button>
+          <button className={styles.button}>Learn More </button>
         </a>
       </div>
       <div className = {styles.blurb}>
@@ -34,25 +32,25 @@ The FIRST Robotics Competition is an international program consisting of thousan
           <li>FIRST UMass Dartmouth District Event Finalist</li>
         </ul>
         <a href = "https://www.youtube.com/watch?v=-SjPVVeNdKY" target="_blank" rel="noopener noreferrer">
-          <button>Explore Our Awards</button>
+          <button className={styles.button}>Explore Our Awards</button>
         </a>
       </div>
       <div className = {styles.blurb}>
         <h2>Sponsors</h2>
         <a href = "https://www.youtube.com/watch?v=-SjPVVeNdKY" target="_blank" rel="noopener noreferrer">
-          <button>Explore Our Sponsors</button>
+          <button className={styles.button}>Explore Our Sponsors</button>
         </a>
       </div>
       <div className = {styles.blurb}>
         <h2>Our Team</h2>
         <a href = "https://www.youtube.com/watch?v=-SjPVVeNdKY" target="_blank" rel="noopener noreferrer">
-          <button>Meet The Team</button>
+          <button className={styles.button}>Meet The Team</button>
         </a>
       </div>
       <div className = {styles.blurb}>
         <h2>Our Newsletter</h2>
         <a href = "https://www.youtube.com/watch?v=-SjPVVeNdKY" target="_blank" rel="noopener noreferrer">
-          <button>Explore The Newsletter</button>
+          <button className={styles.button}>Explore The Newsletter</button>
         </a>
       </div>
     </main>
