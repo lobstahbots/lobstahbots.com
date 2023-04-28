6e6b83792bd298f5822cd85aa948137f7415a3ef
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-04-27 22:00:06 -0400

    add sections

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,29 +1,7 @@
-import styles from "./gallery.module.css";
-import Image from "next/image";
-import lobstahbots from "../images/lobstahbots.png";
 
 export default function Gallery () {
   return (
     <div>
-      <div className={styles.gallery}>
-        <Image src={lobstahbots} alt="default img" width = "500" height = "500"/>
-        <div className={styles.desc}>Add a description of the image here</div>
-      </div>
-
-      <div className={styles.gallery}>
-        <Image src={lobstahbots} alt="default img" width = "500" height = "500"/>
-        <div className={styles.desc}>Add a description of the image here</div>
-      </div>
-
-      <div className={styles.gallery}>
-        <Image src={lobstahbots} alt="default img" width = "500" height = "500"/>
-        <div className={styles.desc}>Add a description of the image here</div>
-      </div>
-
-      <div className={styles.gallery}>
-        <Image src={lobstahbots} alt = "default img" width = "500" height = "500"/>
-        <div className={styles.desc}>Add a description of the image here</div>
-      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./index.module.css";
 import Overlay from "../components/overlay";
 import Image from "next/image";
+import Link from "next/link";
 import teamPhoto from "../images/index/team-photo.jpg";
 
 export default function Page () {
@@ -12,6 +13,49 @@ export default function Page () {
         } content = {
           <p className = {`${styles.photoText} container`}>We are Lobstah Bots, a FIRST Robotics team that aims to bring access to robotics to students in the Greater Boston area.</p>
         }/>
+      </div>
+      <div className = {styles.section}>
+        <div className = "container">
+          <h2>What is the FIRST Robotics Competition?</h2>
+          <div className = {styles.sectionWithCols}>
+            <div className = {styles.leftCol}>
+              <p>
+                FIRST (For Inspiration and Recognition of Science and Technology) was founded in 1992 by Dean Kamen, inventor of the Segway. The mission of FIRST is to inspire young people to be science and technology leaders, by engaging them in exciting programs that build STEM skills and teach self-confidence, communication, and leadership. The FIRST Robotics Competition is an international program consisting of thousands of teams and tens of thousands of students. In FRC, students have just a few weeks to design, build, and code a 100+ pound robot that can compete in a complex engineering challenge. FRC gives students exposure to engineering, technical design, business management, marketing and many more skills that can be used in college and industry. Most FIRST alumni go to a 4-year college and end up employed in the STEM industry.
+              </p>
+              <Link href= "https://www.firstinspires.org/" className = {styles.moreButton}>Explore FIRST</Link>
+            </div>
+
+            <div className = {styles.rightCol}>
+              <Image src = {teamPhoto} alt = "Lobstah Bots Team Photo" className = {styles.teamPhoto}/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className = {styles.section}>
+        <div className = "container">
+          <h2>Explore the 2023 Season</h2>
+          <div className = {styles.sectionWithCols}>
+            <div className = {styles.leftCol}>
+              <h4>
+                <Link href = "https://www.thebluealliance.com/event/2023week0">Event 1: 2023 Week 0 in Nashua, NH</Link>
+              </h4>
+              <h4>
+                <Link href = "https://www.thebluealliance.com/event/2023rinsc">Event 2: 2023 New England District Rhode Island Event</Link>
+              </h4>
+              <h4>
+                <Link href = "https://www.thebluealliance.com/event/2023mabos">Event 3: 2023 New England District Greater Boston Event</Link>
+              </h4>
+              <h4>
+                <Link href = "https://www.thebluealliance.com/event/2023necmp2">Event 4: 2023 New England FIRST District Championships - Wilson Division</Link>
+              </h4>
+              <Link href= "/history" className = {styles.moreButton}>Explore Past Years</Link>
+            </div>
+
+            <div className = {styles.rightCol}>
+              <Image src = {teamPhoto} alt = "Robot Photo" className = {styles.teamPhoto}/>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
