e673660d68cc395f88985208c73d82aa0ff81288
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2023-05-12 15:16:34 -0400

    Move font stylesheet to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
-import Head from "next/head";
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Head>
-        <link rel="stylesheet" href="https://use.typekit.net/blh0nax.css" />
-      </Head>
       <Component {...pageProps} />
     </Layout>
   );
