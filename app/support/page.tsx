39b4be232dabd6e098464915b6e1f65b67b49e3d
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-06-26 20:16:25 -0400

    support pg

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -23,30 +23,30 @@ export default function Support () {
       <div className = "bg-gray">
         <div className = "section container">
           <h1>Sponsorship Tiers</h1>
-          <div className = {styles.tiers}>
-            <div>
-              <h2 className={styles.krill}>Krill</h2>
+          <div>
+            <div className = {`brand-border ${styles.krill}`}>
+              <h2>Krill</h2>
               <div className = {styles.money}>$100+</div>
               <ul className={styles.benefits}><li>Your name and logo will be on our website.</li></ul>
             </div>
-            <div>
-              <h2 className={styles.shrimp}>Shrimp</h2>
+            <div className={`brand-border ${styles.shrimp}`}>
+              <h2>Shrimp</h2>
               <div className={styles.money}>$500+</div>
               <ul className={styles.benefits}>
                 <li>All previous benefits.</li>
                 <li>Your name and logo will be on our team shirt.</li>
               </ul>
             </div>
-            <div>
-              <h2 className={styles.crab}>Crab</h2>
+            <div className={`brand-border ${styles.crab}`}>
+              <h2>Crab</h2>
               <div className={styles.money}>$1000+</div>
               <ul className = {styles.benefits}>
                 <li>All previous benefits.</li>
                 <li>Your name and logo will feature on our robot.</li>
               </ul>
             </div>
-            <div>
-              <h2 className={styles.lobstah}>Lobstah</h2>
+            <div className={`brand-border ${styles.lobstah}`}>
+              <h2>Lobstah</h2>
               <div className={styles.money}>$4000+</div>
               <ul className={styles.benefits}>
                 <li>All previous benefits.</li>
@@ -61,8 +61,8 @@ export default function Support () {
                 event of your choosing.</li>
               </ul>
             </div>
-            <div>
-              <h2 className={styles.blue}>Blue Lobstah</h2>
+            <div className={`brand-border ${styles.blue}`}>
+              <h2>Blue Lobstah</h2>
               <div className={styles.money}>$8000+</div>
               <ul className={styles.benefits}>
                 <li>All previous benefits</li>
