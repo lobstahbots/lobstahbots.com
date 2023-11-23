0c9921579c7ecb2a0a257f716bd14694ab583f4c
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2023-11-23 11:55:56 -0500

    Make footer mobile-friendly, tweak footer spacing

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,30 +7,30 @@ import logo from "../images/logos/claw-logo-white.svg";
 export default function Footer () {
   return (
     <footer className={styles.footer}>
-      <div className="container cols1_1">
-        <div className="cols1_1">
-          <div>
-            <Image className={styles.logo} src={logo} alt="Claw Logo"/>
-          </div>
-          <div>
-            <h4>Copyright &copy; 2023 Lobstah Bots</h4>
+      <div className="container">
+        <div className={styles.mainSection}>
+          <Image className={styles.logo} src={logo} alt="Claw Logo"/>
+          <div className={styles.contactInfo}>
             <p>110 Cummington Mall <br></br>
                 Boston, MA 02215</p>
-            <p>lobstahbots@gmail.com</p>
+            <p><a href="mailto:lobstahbots@gmail.com">lobstahbots@gmail.com</a></p>
           </div>
+          <nav>
+            <ul className={styles.footerLinks}>
+              <li><Link href="/history">Past Seasons</Link></li>
+              <li><Link href="/awards" > Awards </Link></li >
+              <li><Link href="/team">Our Team</Link></li>
+              {/* <li><Link href="/newsletter">Newsletter</Link></li> */}
+              <li><Link href="/sponsors">Sponsors</Link></li>
+              <li><Link href="/gallery">Gallery</Link></li>
+              <li><Link href="/contact">Contact</Link></li>
+              <li><Link href="/support" > Support Us </Link></li >
+            </ul>
+          </nav>
         </div>
-        <div className={styles.footerMenu}>
-          <ul className={styles.footerLinks}>
-            <li><Link href="/history">Past Seasons</Link></li>
-            <li><Link href="/awards" > Awards </Link></li >
-            <li><Link href="/team">Our Team</Link></li>
-            {/* <li><Link href="/newsletter">Newsletter</Link></li> */}
-            <li><Link href="/sponsors">Sponsors</Link></li>
-            <li><Link href="/gallery">Gallery</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
-            <li><Link href="/support" > Support Us </Link></li >
-          </ul>
-        </div>
+      </div>
+      <div className={styles.copyrightSection}>
+        <p className="container">Copyright &copy; 2023 Lobstah Bots</p>
       </div>
     </footer>
   );
