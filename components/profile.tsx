650aa8b5eef3002c6860fa13bcd01454cf3a7371
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2023-12-19 11:37:35 -0500

    Modify Profile component
    Rename ProfileProps.picture to ProfileProps.image, and change it to type
    StaticImageData, which it should be, and add support for multiple roles.

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,20 +1,24 @@
 import styles from "./profile.module.css";
-import Image from "next/image";
+import Image from "next-image-export-optimizer";
+import { StaticImageData } from "next/image";
 
 export interface ProfileProps {
   name: string;
-  role?: string;
-  picture: string;
+  role?: string | string[];
+  image: StaticImageData;
 }
 
-export default function Profile ({ name, role, picture }: ProfileProps) {
+export default function Profile ({ name, role, image }: ProfileProps) {
   return (
     <div className={styles.profile}>
-      <Image className={`responsive-image brand-border ${styles.profilePhoto}`} src={picture} alt="Team Member Profile" />
+      <Image className={`responsive-image brand-border ${styles.profilePhoto}`} src={image} alt="Team Member Profile" />
       <div className={styles.name}>{name}</div>
       {
         role && (
-          <div className={styles.role}>{role}</div>
+          <div className={styles.role}>{
+            Array.isArray(role)
+              ? role.flatMap(str => [str, <br/>]).slice(0, -1)
+              : role}</div>
         )
       }
     </div>
