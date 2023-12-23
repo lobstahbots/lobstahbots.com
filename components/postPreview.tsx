6ba55f67af48c764988876dae63c7457a50d0e11
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-12-23 06:14:01 -0500

    use client

--- components/postPreview.tsx
+++ components/postPreview.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./postPreview.module.css";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,7 +13,7 @@ type Items = {
 export default function PostPreview({ post }: { post: Items }) {
   const router = useRouter();
   router.push(`/blog/${post.slug}`);
-  
+
   return (
     <div className = {styles.preview}>
       <Link className = {styles.title} href={ `/blog/${post.slug}` }> {post.title}
