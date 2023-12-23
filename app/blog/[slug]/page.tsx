ea9ddbaaecf0df90f534b50df21600a36c32efae
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-12-23 08:03:03 -0500

    event handler

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -11,13 +11,17 @@ export default async function Post ({ params }: { params: { slug: string } }) {
   //   params.slug = params.slug.concat(".md");
   // }
 
+  function displaySlug() {
+    console.log(params.slug);
+  }
+
   const post = getPostBySlug(params.slug, ["title", "author", "content", "date"]);
 
 
   const content = await markdownToHtml(post["content"] || "");
 
   return (
-    <div onClick = {() => { console.log(params.slug) }} className = "section container">
+    <div onClick = {displaySlug()} className = "section container">
       <div className = { styles.logoLine }><Image className = {`responsive-image ${styles.logo}`} width = "564" height = "564" id="2" alt = "Logo" src = "https://mcusercontent.com/b62ff52e31b232a8696b02d06/images/5d14253b-cfaf-9ad4-6bb2-c09779fc9abd.png"/></div>
       <div className = {styles.newsletterName}> <div id="17"> <p> <strong><span> NEWSLETTER </span><span className = {styles.vert}><span>|</span > </span><span className = {styles.date}> {post.date} </span> </strong></p> </div></div>    
       <div className = {styles.breakLine} > </div>
