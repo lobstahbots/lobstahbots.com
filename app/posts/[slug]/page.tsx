c358f081d8ebe619ca9365b12c2e0bac5716b2f0
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-12-23 02:00:28 -0500

    html layout

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import { getPostBySlug } from "../../../lib/posts";
+import markdownToHtml from "../../../lib/markdownToHTML";
+import markdownStyles from "./markdown-styles.module.css";
+import styles from "./styles.module.css";
+import Image from "next-image-export-optimizer";
+
+export default async function Post({ params }: { params: { slug: string } }) {
+  const post = getPostBySlug(params.slug, ["title", "author", "content", "date"]);
+
+  const content = await markdownToHtml(post["content"] || "");
+
+  return (
+    <div className = "section container">
+      <div className = { styles.logoLine }><Image className = {`responsive-image ${styles.logo}`} width = "564" height = "564" id="2" alt = "Logo" src = "https://mcusercontent.com/b62ff52e31b232a8696b02d06/images/5d14253b-cfaf-9ad4-6bb2-c09779fc9abd.png"/></div>
+      <div className = {styles.newsletterName}> <div id="17"> <p> <strong><span> NEWSLETTER </span><span className = {styles.vert}><span>|</span > </span><span> {post.date} </span> </strong></p> </div></div>    
+      <div className = {styles.breakLine} > </div>
+      <div className = {styles.content}> 
+        <h1 className = {styles.title} > {post.title} </h1>
+        < div className = { markdownStyles["markdown"]} dangerouslySetInnerHTML = {{ __html: content }} />
+        </div>
+      </div>
+  );
+}
