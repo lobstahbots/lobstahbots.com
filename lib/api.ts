7bd493286331270a59ab089199e4cb44262ef605
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2023-12-25 12:12:35 -0500

    Add previous post and next post functions

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,10 @@ import * as fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
+type Items = {
+  [key: string]: string;
+};
+
 const postsDirectory = path.join(process.cwd(), "_posts");
 
 export function getPostSlugs() {
@@ -13,10 +17,6 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
   const fullPath = path.join(postsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
-
-  type Items = {
-    [key: string]: string;
-  };
 
   const items: Items = {};
 
@@ -35,15 +35,42 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
   });
 
   return items;
-} 
+}
 
 export function getAllPosts(fields: string[] = []) {
-  fields = ["title", "content", "author", "date", "numericalDate", "slug", "type", "coverImage"];
+  fields = [
+    "title",
+    "content",
+    "author",
+    "date",
+    "numericalDate",
+    "slug",
+    "type",
+    "coverImage",
+  ];
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
-    .sort((post1, post2) => (post1.numericalDate > post2.numericalDate ? -1 : 1));
+    .sort((post1, post2) =>
+      post1.numericalDate > post2.numericalDate ? -1 : 1
+    );
 
   return posts;
+}
+
+export function getPreviousPost(slug: string): Items | undefined {
+  const posts = getAllPosts();
+  const currentPostIndex = posts.findIndex((post) => post.slug === slug);
+  if (0 <= currentPostIndex && currentPostIndex < posts.length) {
+    return posts[currentPostIndex + 1];
+  }
+}
+
+export function getNextPost(slug: string): Items | undefined {
+  const posts = getAllPosts();
+  const currentPostIndex = posts.findIndex((post) => post.slug === slug);
+  if (0 < currentPostIndex && currentPostIndex <= posts.length) {
+    return posts[currentPostIndex - 1];
+  }
 }
