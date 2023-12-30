d46fe0de1d75063c58b24bc5871ede8383c0c7ec
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2023-12-30 16:37:06 -0500

    Cache images for longer
    Previously, images used the default cache TTL which was 600 seconds (!), way too short of a time. This changes it to a year, a usually recommended time.

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     loader: "custom",
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    minimumCacheTTL: 31536000,
   },
   transpilePackages: ["next-image-export-optimizer"],
   env: {
