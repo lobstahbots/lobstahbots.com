1de868e965d66a2e9b557bdd69572fc91dee92d6
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-05-11 10:23:55 -0400

    dynamic copyright notice

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import logo from "../images/logos/claw-logo-white.svg";
 
 
-export default function Footer () {
+export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className="container">
@@ -15,29 +15,28 @@ export default function Footer () {
                 Boston, MA 02215</p>
             <p><a href="mailto:lobstahbots@gmail.com">lobstahbots@gmail.com</a></p>
           </div>
-          <nav className = "cols1_1">
+          <nav className="cols1_1">
             <div>
               <ul className={styles.footerLinks}>
-                <li className = {styles.footerLink}><Link href="/blog" > Posts </Link></li>
-                <li className = {styles.footerLink}><Link href="/history">Past Seasons</Link></li>
-                <li className = {styles.footerLink}><Link href="/awards" > Awards </Link></li >
-                <li className = {styles.footerLink}><Link href="/team">Our Team</Link></li>
+                <li className={styles.footerLink}><Link href="/blog">Posts</Link></li>
+                <li className={styles.footerLink}><Link href="/history">Past Seasons</Link></li>
+                <li className={styles.footerLink}><Link href="/awards">Awards</Link></li >
+                <li className={styles.footerLink}><Link href="/team">Our Team</Link></li>
               </ul>
             </div>
             <div>
               <ul className={styles.footerLinks}>
-                <li className = {styles.footerLink}><Link href="/sponsors">Sponsors</Link></li>
-                <li className = {styles.footerLink}><Link href="/gallery">Gallery</Link></li>
-                <li className = {styles.footerLink}><Link href="/contact">Contact</Link></li>
-                <li className = {styles.footerLink}><Link href="/support" > Support Us </Link></li >
+                <li className={styles.footerLink}><Link href="/sponsors">Sponsors</Link></li>
+                <li className={styles.footerLink}><Link href="/gallery">Gallery</Link></li>
+                <li className={styles.footerLink}><Link href="/contact">Contact</Link></li>
+                <li className={styles.footerLink}><Link href="/support">Support Us</Link></li >
               </ul>
             </div>
-
           </nav>
         </div>
       </div>
       <div className={styles.copyrightSection}>
-        <p className="container">Copyright &copy; 2023 Lobstah Bots</p>
+        <p className="container">Copyright &copy; 2023&ndash;{new Date().getFullYear()} Lobstah Bots</p>
       </div>
     </footer>
   );
