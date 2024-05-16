a48a53b417b0a5654848153746dd2b2d44fc69b0
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-05-16 14:36:55 -0400

    swap order

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -14,8 +14,7 @@ export default function Sponsors () {
         <PageTitle>Sponsors</PageTitle>
         <p className = {styles.sponsors}> The contributions of sponsors are critical to the success of our team. Our sponsors are true champions of education and innovation. They provide financial and material support that enables us to build and program robots, travel to competitions, and positively impact our community. By investing in Team 246, they support the development of leaders and problem solvers who will shape the world of tomorrow, one robot at a time. </p>
       </div>
-      <SponsorsSection />
-      <div className="bg-brand">
+       <div className="bg-brand">
         <div className="section container">
           <h1>How Can You Help?</h1>
           <p >As a FIRST Robotics team, we rely on the support of our community to fund our robot builds, travel expenses, and outreach programs. We invite you to support the Lobstah Bots by making a donation! Your donation will help us continue our mission of providing students from all of Greater Boston with the opportunity to participate in the exciting and challenging world of robotics. From material to financial, contributions of any form are a huge support to our team.</p>
@@ -26,6 +25,8 @@ export default function Sponsors () {
           </div>
         </div>
       </div>
+      <SponsorsSection />
+     
     </main>
   );
 }
