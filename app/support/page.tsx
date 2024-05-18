b2c81a0dce3e0fdd5ccc52314b3404e07d753cbf
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-05-18 08:03:22 -0400

    those should be ids + shared should be a class
    also the benefits were still black so I made them white

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -24,12 +24,12 @@ export default function Support () {
         <div className = "section container">
           <h1>Sponsorship Tiers</h1>
           <div>
-            <div className = {`brand-border ${styles.krill}`}>
+            <div className = {`brand-border ${styles.sponsorTier}`} id={styles.krill}>
               <h2>Krill</h2>
               <div className = {styles.money}>$100+</div>
               <ul className={styles.benefits}><li>Your name and logo will be on our website.</li></ul>
             </div>
-            <div className={`brand-border ${styles.shrimp}`}>
+            <div className={`brand-border ${styles.sponsorTier}`} id={styles.shrimp}>
               <h2>Shrimp</h2>
               <div className={styles.money}>$500+</div>
               <ul className={styles.benefits}>
@@ -37,7 +37,7 @@ export default function Support () {
                 <li>Your name and logo will be on our team shirt.</li>
               </ul>
             </div>
-            <div className={`brand-border ${styles.crab}`}>
+            <div className={`brand-border ${styles.sponsorTier}`} id={styles.crab}>
               <h2>Crab</h2>
               <div className={styles.money}>$1000+</div>
               <ul className = {styles.benefits}>
@@ -45,7 +45,7 @@ export default function Support () {
                 <li>Your name and logo will feature on our robot.</li>
               </ul>
             </div>
-            <div className={`brand-border ${styles.lobstah}`}>
+            <div className={`brand-border ${styles.sponsorTier}`} id={styles.lobstah}>
               <h2>Lobstah</h2>
               <div className={styles.money}>$4000+</div>
               <ul className={styles.benefits}>
@@ -61,7 +61,7 @@ export default function Support () {
                 event of your choosing.</li>
               </ul>
             </div>
-            <div className={`brand-border ${styles.blue}`}>
+            <div className={`brand-border ${styles.sponsorTier}`} id={styles.blueLobstah}>
               <h2>Blue Lobstah</h2>
               <div className={styles.money}>$8000+</div>
               <ul className={styles.benefits}>
