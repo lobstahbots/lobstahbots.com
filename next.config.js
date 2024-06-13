967ecd766f12899ed3514c8ab51021555c2fa304
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-06-12 21:28:22 -0400

    don't export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: "export",
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
