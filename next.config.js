e3a4bba267e05a122b3a01546768c03cbe585a48
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-06-12 22:04:54 -0400

    make images higher res

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     minimumCacheTTL: 31536000,
   },
   eslint: {
