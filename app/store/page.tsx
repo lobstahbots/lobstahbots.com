f1008011c2fd7d8b8fe3cadf04ee66216ee9398d
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-06-19 09:40:10 +0300

    fix error where shop loads but errors

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -9,12 +9,6 @@ export default function Store() {
   return (
     <>
       <main>
-        <script id="spread_shop_config" dangerouslySetInnerHTML={ { __html:
-        `var spread_shop_config = {
-	shopName: 'lobstahbots',
-	prefix: 'https://lobstahbots.myspreadshop.com',
-	baseId: 'lobstahbotsStore',
-};` }}/>
         <div id="lobstahbotsStore">Loading store...</div>
       </main>
       <Script type="text/javascript" src="/js/shop.js" strategy="lazyOnload" />
