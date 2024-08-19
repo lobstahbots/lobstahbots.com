47ce8d10b530b9d398debbb56866274f43fd506c
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-08-19 12:36:57 -0400

    get past seasions automatically from tba

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -11,28 +11,16 @@ export const metadata = {
   title: "Past Seasons",
 };
 
-export const years: YearProps[] = [
+export const yearPropOverrides: Pick<
+  YearProps,
+  "year" | "link" | "title" | "robotName" | "robotImage"
+>[] = [
   {
     year: 2024,
     link: "https://www.youtube.com/watch?v=9keeDyFxzY4",
     title: "Crescendo",
     robotName: "Woodpecker",
     robotImage: woodpecker,
-    events: [
-      {
-        link: "https://www.thebluealliance.com/event/2024mabri",
-        name: "2024 New England District BSU Event",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2024mabos",
-        name: "2024 New England District Greater Boston Event",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2024necmp1",
-        name: "2024 New England FIRST District Championships - Ganson Division",
-      },
-    ],
-    offseasonEvents: [],
   },
   {
     year: 2023,
@@ -40,34 +28,6 @@ export const years: YearProps[] = [
     title: "Charged Up",
     robotName: "Llama",
     robotImage: llama,
-    events: [
-      {
-        link: "https://www.thebluealliance.com/event/2023week0",
-        name: "2023 Week 0 in Nashua, NH",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2023rinsc",
-        name: "2023 New England District Rhode Island Event",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2023mabos",
-        name: "2023 New England District Greater Boston Event",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2023necmp2",
-        name: "2023 New England FIRST District Championships - Wilson Division",
-      },
-    ],
-    offseasonEvents: [
-      {
-        link: "https://www.thebluealliance.com/event/2023nhmm",
-        name: "2023 Mayhem in Merrimack, NH",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2023matb",
-        name: "2023 New England Robotics Derby (N.E.R.D.)",
-      },
-    ],
   },
   {
     year: 2022,
@@ -75,21 +35,6 @@ export const years: YearProps[] = [
     title: "Rapid React",
     robotName: "Lobstah",
     robotImage: lobstah,
-    events: [
-      {
-        link: "https://www.thebluealliance.com/event/2022week0",
-        name: "2022 Week 0 in Bedford, NH",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2022marea",
-        name: "2022 New England District North Shore Event",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2022nhsea",
-        name: "2022 New England District Pease ANG Event",
-      },
-    ],
-    offseasonEvents: [],
   },
   {
     year: 2020,
@@ -97,17 +42,6 @@ export const years: YearProps[] = [
     title: "Infinite Recharge",
     robotName: "Popcorn",
     robotImage: popcorn,
-    events: [
-      {
-        link: "https://www.thebluealliance.com/event/2020week0",
-        name: "2020 Week 0 in Nashua, NH",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2020mabri",
-        name: "2020 New England District SE Mass Event",
-      },
-    ],
-    offseasonEvents: [],
   },
   {
     year: 2019,
@@ -115,37 +49,83 @@ export const years: YearProps[] = [
     title: "Deep Space",
     robotName: "Burrito",
     robotImage: burrito,
-    events: [
-      {
-        link: "https://www.thebluealliance.com/event/2019week0",
-        name: "2019 Week 0 in Merrimack, NH",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2019mabri",
-        name: "2019 New England District SE Mass Event",
-      },
-      {
-        link: "https://www.thebluealliance.com/event/2019mabos",
-        name: "2019 New England District Greater Boston Event",
-      },
-    ],
-    offseasonEvents: [
-      {
-        link: "https://www.thebluealliance.com/event/2019bt",
-        name: "2019 Beantown Blitz in Revere, MA",
-      },
-    ],
   },
 ];
 
-export default function History() {
+const FIRST_YEAR = 1999;
+const START_YEAR = 2019;
+const EXCLUDE_YEARS = [2021];
+export const TEAM_KEY = "frc246";
+const AWARDS_URL = `https://www.thebluealliance.com/api/v3/team/${TEAM_KEY}/awards`;
+const EVENTS_URL = `https://www.thebluealliance.com/api/v3/team/${TEAM_KEY}/events/simple`;
+export const headers = new Headers();
+headers.append("X-TBA-Auth-Key", process.env.TBA_AUTH_KEY!);
+headers.append("Accept", "application/json");
+
+export interface SimpleEvent {
+  key: string;
+  name: string;
+  event_type: number;
+  year: number;
+  start_date: string;
+}
+
+export const toEventsList = (events: SimpleEvent[]) => {
+  return events
+    .filter((event) => !event.name.toLowerCase().includes("cancelled"))
+    .sort((eventA, eventB) => eventA.start_date.localeCompare(eventB.start_date))
+    .map((event) => ({
+      name: event.year + " " + event.name.replace("NE", "New England"),
+      link: `https://www.thebluealliance.com/event/${event.key}`,
+    }));
+};
+
+export default async function History() {
+  const [awardsResponse, eventsResponse] = await Promise.all([
+    fetch(AWARDS_URL, { headers, cache: "force-cache" }),
+    fetch(EVENTS_URL, { headers, cache: "force-cache" }),
+  ]);
+  const [awards, events]: [any[], SimpleEvent[]] = await Promise.all([
+    awardsResponse.json(),
+    eventsResponse.json(),
+  ]);
+  const years: YearProps[] = [];
+  for (let year = new Date().getFullYear(); year >= START_YEAR; year--) {
+    if (EXCLUDE_YEARS.includes(year)) {
+      continue;
+    }
+    years.push({
+      events: toEventsList(
+        events.filter((event) => event.year === year && event.event_type !== 99),
+      ),
+      offseasonEvents: toEventsList(
+        events.filter((event) => event.year === year && event.event_type === 99),
+      ),
+      ...(yearPropOverrides.find((override) => override.year === year) ?? {
+        link: "",
+        title: year.toString(),
+        robotName: "Robot",
+        robotImage: lobstah,
+        year,
+      }),
+    });
+  }
+  const yearsCompeted = new Date().getFullYear() - FIRST_YEAR + 1;
+  const awardsEarned = awards.length;
+  const eventsAttended = events.length;
   return (
     <main>
       <PageTitle>Past Seasons</PageTitle>
-      <Numbers yearsCompeted={26} awardsEarned={25} eventsAttended={55} />
-      {years.map((year, index) => (
-        <Year {...year} key={year.year} className={index % 2 === 0 ? "bg-gray" : undefined} />
-      ))}
+      <Numbers
+        yearsCompeted={yearsCompeted}
+        awardsEarned={awardsEarned}
+        eventsAttended={eventsAttended}
+      />
+      {years
+        .sort((yearA, yearB) => yearB.year - yearA.year)
+        .map((year, index) => (
+          <Year {...year} key={year.year} className={index % 2 === 0 ? "bg-gray" : undefined} />
+        ))}
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,25 @@ import { ExternalLink, Instagram, Youtube, ArrowRight, GitHub } from "react-feat
 import SponsorsSection from "../components/sponsors-section";
 import { getAllPosts } from "../lib/api";
 import PostPreview from "../components/postPreview";
-import { years } from "./history/page";
+import {
+  yearPropOverrides as years,
+  headers,
+  TEAM_KEY,
+  SimpleEvent,
+  toEventsList,
+} from "./history/page";
 
-export default function Page () {
+export default async function Page() {
   const data = getAllPosts(["title", "date", "excerpt", "coverImage", "slug"]);
   const posts = data.slice(0, 3);
   const thisYear = [...years].sort((a, b) => b.year - a.year)[0];
+
+  const eventsResponse = await fetch(
+    `https://www.thebluealliance.com/api/v3/team/${TEAM_KEY}/events/${thisYear.year}/simple`,
+    { headers, cache: "force-cache" },
+  );
+
+  const events = toEventsList((await eventsResponse.json()) as SimpleEvent[]);
 
   return (
     <main>
@@ -24,23 +37,44 @@ export default function Page () {
         <Overlay>
           <Image src={teamSelfie} alt="Lobstah Bots Team Photo" className={styles.heroBackground} />
           <div className="container">
-            <p className={styles.heroText}>We are <strong>Lobstah Bots</strong>, A Boston-based <i>FIRST</i>® Robotics team developing skills and creating memories in a collaborative environment.</p>
+            <p className={styles.heroText}>
+              We are <strong>Lobstah Bots</strong>, A Boston-based <i>FIRST</i>® Robotics team
+              developing skills and creating memories in a collaborative environment.
+            </p>
           </div>
         </Overlay>
       </header>
       <section className="section container">
-        <h1>What is the <i>FIRST</i> Robotics Competition?</h1>
+        <h1>
+          What is the <i>FIRST</i> Robotics Competition?
+        </h1>
         <div className="cols1_1">
           <div>
             <p>
-              <i>FIRST</i> (For Inspiration and Recognition of Science and Technology) was founded in 1992 by Dean Kamen, inventor of the Segway. The mission of <i>FIRST</i> is to inspire young people to be science and technology leaders, by engaging them in exciting programs that build STEM skills and teach self-confidence, communication, and leadership. The <i>FIRST</i> Robotics Competition is an international program consisting of thousands of teams and tens of thousands of students. In FRC, students have just a few weeks to design, build, and code a 100+ pound robot that can compete in a complex engineering challenge. FRC gives students exposure to engineering, technical design, business management, marketing and many more skills that can be used in college and industry. Most <i>FIRST</i> alumni go to a 4-year college and end up employed in the STEM industry.
+              <i>FIRST</i> (For Inspiration and Recognition of Science and Technology) was founded
+              in 1992 by Dean Kamen, inventor of the Segway. The mission of <i>FIRST</i> is to
+              inspire young people to be science and technology leaders, by engaging them in
+              exciting programs that build STEM skills and teach self-confidence, communication, and
+              leadership. The <i>FIRST</i> Robotics Competition is an international program
+              consisting of thousands of teams and tens of thousands of students. In FRC, students
+              have just a few weeks to design, build, and code a 100+ pound robot that can compete
+              in a complex engineering challenge. FRC gives students exposure to engineering,
+              technical design, business management, marketing and many more skills that can be used
+              in college and industry. Most <i>FIRST</i> alumni go to a 4-year college and end up
+              employed in the STEM industry.
             </p>
             <div className={styles.rightButtonRow}>
-              <Link href="https://www.firstinspires.org/" className="button">Explore FIRST</Link>
+              <Link href="https://www.firstinspires.org/" className="button">
+                Explore FIRST
+              </Link>
             </div>
           </div>
           <div>
-            <Image src={matchPhoto} alt="District Champs Match Photo" className="responsive-image brand-border" />
+            <Image
+              src={matchPhoto}
+              alt="District Champs Match Photo"
+              className="responsive-image brand-border"
+            />
           </div>
         </div>
       </section>
@@ -48,55 +82,77 @@ export default function Page () {
         <h1>Explore the {thisYear.year} Season</h1>
         <div className="cols1_2">
           <div>
-            <Image src={eventPhoto} alt={`${thisYear.year} Event Photo`} className="responsive-image brand-border" />
+            <Image
+              src={eventPhoto}
+              alt={`${thisYear.year} Event Photo`}
+              className="responsive-image brand-border"
+            />
           </div>
-          <div className = {styles.event}>
+          <div className={styles.event}>
             <div className={styles.eventLinks}>
-              {
-                thisYear.events.map((event, index) => (
-                  <div className={styles.link} key={event.link}>
-                    <h3>Event {index + 1}:</h3>
-                    <Link href={event.link} className="link">{event.name}</Link>
-                  </div>
-                ))
-              }
+              {events.map((event, index) => (
+                <div className={styles.link} key={event.link}>
+                  <h3>Event {index + 1}:</h3>
+                  <Link href={event.link} className="link">
+                    {event.name}
+                  </Link>
+                </div>
+              ))}
             </div>
             <div className={styles.buttons}>
               <div className={styles.leftButtonRow}>
-                <Link href={thisYear.link} className="button" > {thisYear.year} Season Game Reveal < ExternalLink /> </Link>
-                <Link href="/history" className="button">Past Events <ArrowRight /></Link>
-                <Link href="/awards" className="button"> Check Out Our Awards< ArrowRight /> </Link>
+                <Link href={thisYear.link} className="button">
+                  {" "}
+                  {thisYear.year} Season Game Reveal <ExternalLink />{" "}
+                </Link>
+                <Link href="/history" className="button">
+                  Past Events <ArrowRight />
+                </Link>
+                <Link href="/awards" className="button">
+                  {" "}
+                  Check Out Our Awards
+                  <ArrowRight />{" "}
+                </Link>
               </div>
             </div>
           </div>
         </div>
-
       </section>
       <section className="bg-gray">
         <div className="container section">
           <h1>See The Latest</h1>
-          <div className = { styles.posts } >
-            {
-              posts.map((post) => (
-                <div className = {styles.postCard} key={post.slug}>
-                  <PostPreview post= { post } />
-                </div>
-              ))
-            }
+          <div className={styles.posts}>
+            {posts.map((post) => (
+              <div className={styles.postCard} key={post.slug}>
+                <PostPreview post={post} />
+              </div>
+            ))}
           </div>
 
-          <div className = {styles.buttonRow}>
-            <div className = {styles.leftButtonRow}>
-              <div className = {styles.follow}>Follow Us:</div>
+          <div className={styles.buttonRow}>
+            <div className={styles.leftButtonRow}>
+              <div className={styles.follow}>Follow Us:</div>
               <div className={styles.mediaIcons}>
-                <Link href="https://www.instagram.com/burobotics246/?hl=en" target="_blank"><Instagram className={styles.mediaIcon} /></Link>
-                <Link href="https://www.youtube.com/@burobotics/" target="_blank"><Youtube className={styles.mediaIcon} /></Link>
-                <Link href="https://www.github.com/lobstahbots/" target="_blank"><GitHub className={styles.mediaIcon} /></Link>
+                <Link href="https://www.instagram.com/burobotics246/?hl=en" target="_blank">
+                  <Instagram className={styles.mediaIcon} />
+                </Link>
+                <Link href="https://www.youtube.com/@burobotics/" target="_blank">
+                  <Youtube className={styles.mediaIcon} />
+                </Link>
+                <Link href="https://www.github.com/lobstahbots/" target="_blank">
+                  <GitHub className={styles.mediaIcon} />
+                </Link>
               </div>
             </div>
             <div className={styles.rightButtonRow}>
-              <Link href="/blog" className="button"> More Posts <ArrowRight /></Link>
-              <Link href="/gallery" className="button"> Team Gallery <ArrowRight /></Link>
+              <Link href="/blog" className="button">
+                {" "}
+                More Posts <ArrowRight />
+              </Link>
+              <Link href="/gallery" className="button">
+                {" "}
+                Team Gallery <ArrowRight />
+              </Link>
             </div>
           </div>
         </div>
@@ -105,10 +161,16 @@ export default function Page () {
         <div>
           <h1>Our Members Matter</h1>
           <div>
-            <p>What would a team be without its dedicated members? From freshman to senior year, high school students from all over the Greater Boston area collaborate, innovate, and cultivate a variety of practical real-world skills on Team 246.</p>
+            <p>
+              What would a team be without its dedicated members? From freshman to senior year, high
+              school students from all over the Greater Boston area collaborate, innovate, and
+              cultivate a variety of practical real-world skills on Team 246.
+            </p>
           </div>
           <div className={styles.leftButtonRow}>
-            <Link href="/team" className="button">Meet The Team <ArrowRight /></Link>
+            <Link href="/team" className="button">
+              Meet The Team <ArrowRight />
+            </Link>
           </div>
         </div>
         <div>
@@ -119,14 +181,35 @@ export default function Page () {
         <div className="section container cols1_2">
           <div>
             <h1>Support Us!</h1>
-            <Link href="https://drive.google.com/file/d/1RZfQazIUt9vKnLXxb56axNBI7H7Gtghv/view?usp=sharing" target="_blank" className={styles.sponsorUs}>Sponsorship Information</Link>
+            <Link
+              href="https://drive.google.com/file/d/1RZfQazIUt9vKnLXxb56axNBI7H7Gtghv/view?usp=sharing"
+              target="_blank"
+              className={styles.sponsorUs}
+            >
+              Sponsorship Information
+            </Link>
           </div>
           <div className={styles.textBlock}>
-            <p>We rely on sponsors to provide the funds we need to build robots and run the team. Donations are used to purchase tools and materials, pay for competition registration fees, subsidize travel costs for students attending competitions, promote STEM in the local community, and much more. Without our sponsors, the team could not exist. For more information, please reach out!</p>
+            <p>
+              We rely on sponsors to provide the funds we need to build robots and run the team.
+              Donations are used to purchase tools and materials, pay for competition registration
+              fees, subsidize travel costs for students attending competitions, promote STEM in the
+              local community, and much more. Without our sponsors, the team could not exist. For
+              more information, please reach out!
+            </p>
             <div className={styles.rightButtonRow}>
-              <Link href="/contact" className="button-white">Contact</Link>
-              <Link href="https://trusted.bu.edu/s/1759/2-bu/giving/interior.aspx?sid=1759&gid=2&pgid=3962&cid=7331&dids=359&bledit=1&appealcode=WEBBUA" className="button-white">Donate</Link>
-              <Link href="/store" className="button-white">Shop Our Store</Link>
+              <Link href="/contact" className="button-white">
+                Contact
+              </Link>
+              <Link
+                href="https://trusted.bu.edu/s/1759/2-bu/giving/interior.aspx?sid=1759&gid=2&pgid=3962&cid=7331&dids=359&bledit=1&appealcode=WEBBUA"
+                className="button-white"
+              >
+                Donate
+              </Link>
+              <Link href="/store" className="button-white">
+                Shop Our Store
+              </Link>
             </div>
           </div>
         </div>
