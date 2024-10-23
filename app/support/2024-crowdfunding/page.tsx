7ae60f900c32bf27c00f506348374f2e5c958cc3
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2024-10-22 21:40:23 -0400

    Fix things

--- app/support/2024-crowdfunding/page.tsx
+++ app/support/2024-crowdfunding/page.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import styles from "./styles.module.css";
 import useCountdown from "../../../lib/useCountdown";
+import Link from "next/link";
 
 export default function Page() {
-  const countdown = useCountdown(new Date(1730199600000))
+  const countdown = useCountdown(new Date(1730174400000));
 
   return (
     <main>
@@ -10,7 +13,7 @@ export default function Page() {
         <h1>Get ready for the 2024 crowdfunding campaign!</h1>
         <p>2…4…6… and we’re a go! The Lobstah Bots are back at it! Our crowdfunding campaign is kicking on October 27. Spread the word, stay tuned for some furious fundraising, and check back at this page.</p>
       </div>
-      <div className="bg-red">
+      <div className="bg-brand">
         <div className="section container">
           <div className={styles.countdown}>
             <div className={styles.column}>
@@ -26,7 +29,12 @@ export default function Page() {
               <div className={styles.unitLabel}> Minutes </div>
             </div>
           </div>
-          <p className={styles.tagline}>before the start of the crowdfunding!</p>
+        </div>
+      </div>
+      <div className="container">
+        <p className={styles.tagline}>...before the start of the campaign!</p>
+        <div className={`section ${styles.right}`}>
+          <Link className="button" href="/">Learn More About Our Team</Link>
         </div>
       </div>
     </main>
